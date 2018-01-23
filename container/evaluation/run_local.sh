6dd03bfe7146fde0c44cf1df1324a6b8dbb200c0
Lorenzo <Lorenzo.toscano@gmail.com>
2018-01-23 15:52:17 +0100

    Update run_local.sh

--- container/evaluation/run_local.sh
+++ container/evaluation/run_local.sh
@@ -1,11 +1,11 @@
 docker run \
--v /home/ec2-user/SageMaker/CNTK_CUSTOM_CONTAINER/container/setup:/data \
--v /home/ec2-user/SageMaker/CNTK_CUSTOM_CONTAINER/container/context:/opt/ml \
+-v /home/ec2-user/SageMaker/CNTK_KERAS_SAGEMAKER/container/setup:/data \
+-v /home/ec2-user/SageMaker/CNTK_KERAS_SAGEMAKER/container/context:/opt/ml \
 -p 8080:8080 \
 --rm \
 cntkdemo \
 $1
-# if you are testing locally, to avoid continous image rebuilds
-# replace $1 
+# if you are testing locally, to avoid continous image rebuilds replace
+# $1 
 # with 
 # python /data/$1
